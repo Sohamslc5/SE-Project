f80feef86b0a4ebe25115cff86b8abf473f42a8c
sohamslc5@gmail.com <sohamslc5@gmail.com>
2023-05-04 10:24:15 +0530

    file structure improved

--- routes/publications.route.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const addNewPost = require("../controllers/publication.js");
-router.post("/newpost", addNewPost);
-module.exports = router;
